8e3901892d94210c43b418b46a875ac22068acbf
mixereqv1 <mateuszral7@gmail.com>
2019-08-27 00:00:50 +0200

    First upload...

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const tabs_switcher = {
+    btns: document.querySelectorAll('.btn__tab'),
+    tabs: document.querySelectorAll('.main__tab'),
+    switch () {
+        for (let i = 0; i < this.btns.length; i++) {
+            this.btns[i].addEventListener('click', e => {
+                for (let j = 0; j < this.btns.length; j++) {
+                    this.btns[j].classList.remove('active__btn', 'border__right', 'border__left', 'border__both');
+                    this.tabs[j].classList.remove('active__tab');
+                }
+                this.btns[i].classList.add('active__btn');
+                this.tabs[i].classList.add('active__tab');
+                
+                if (i === 0) {
+                    this.btns[1].classList.add('border__left');
+                    this.btns[2].classList.add('border__left');
+                } else if (i === 1) {
+                    this.btns[0].classList.add('border__right');
+                    this.btns[2].classList.add('border__left');
+                } else {
+                    this.btns[0].classList.add('border__right');
+                    this.btns[1].classList.add('border__right');
+                }
+            })
+        }
+    }
+}
+tabs_switcher.switch();
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Mateusz Ral - portfolio
+## Used technologies:
+- HTML5, SCSS, Pure JS, animate.css
+## Version 1.0
